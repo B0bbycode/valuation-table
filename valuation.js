78beba48f507f89ffa0b59505ba44e3d3a51fb01
B0bbycode <onaolapolekki@outlook.com>
2023-02-23 16:30:45 +0200

    Examine Update

--- valuation.js
+++ valuation.js
@@ -9,6 +9,9 @@ const preMoneyValuationInput = document.getElementById("pre-amount");
 // Get the result element
 const resultElement = document.getElementById("result");
 
+// Get the clear input button
+const clearInputButton = document.getElementById("clear-input");
+
 // Add submit event listener to the form
 form.addEventListener("submit", function(event) {
     event.preventDefault(); // Prevent the form from submitting
@@ -24,8 +27,24 @@ form.addEventListener("submit", function(event) {
     const perShareUnit = (amountRaised / (ownershipPercentage));
 
     // Format the result message
-    const resultMessage = `Dear ${companyName}, you will be required to give away ${ownershipPercentage.toFixed(2)}% worth of equity for raising $${amountRaised.toLocaleString()} at a pre-money valuation of $${preMoneyValuation.toLocaleString()}.`;
+    const resultMessage = `Dear ${companyName}, you will be required to give away ${ownershipPercentage.toFixed(2)}% worth of equity for raising £${amountRaised.toLocaleString()} at a pre-money valuation of £${preMoneyValuation.toLocaleString()}.`;
 
     // Set the result message in the result element
     resultElement.innerText = resultMessage;
+
+    // Check if all input fields are empty, and disable the clear input button if they are
+    if (companyName === "" && amountRaised === "" && preMoneyValuation === "") {
+        clearInputButton.disabled = true;
+    } else {
+        clearInputButton.disabled = false;
+    }
+
+    // Add click event listener to the clear input button
+    clearInputButton.addEventListener("click", function() {
+        companyNameInput.value = "";
+        amountRaisedInput.value = "";
+        preMoneyValuationInput.value = "";
+        clearInputButton.disabled = true; // Disable the button after clearing the input fields
+    });
+
 });
